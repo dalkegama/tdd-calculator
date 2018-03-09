10eac3cbf0cd1602da1a50ccb94a104d0f9c8d62
Ondrej Urik <ondrej.urik@live.com>
2018-03-09 15:34:53 +0000

    Added sources

---
+++ tdd.calculator.tests/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace tdd.calculator.tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_WhenInputIsNull_ShouldReturnZero()
+        {
+            const string numbers = null;
+
+            var result = Calculator.Add(numbers);
+
+            result.ShouldBe(0);
+        }
+
+        [Test]
+        public void Add_WhenInputIsAnumber_ShouldReturnTheNumber()
+        {
+            var result = Calculator.Add("1");
+
+            result.ShouldBe(1);
+        }
+
+        [Test]
+        public void Add_WhenInputIsTwoNumbers_ShouldReturnSum()
+        {
+            var result = Calculator.Add("1,2");
+
+            result.ShouldBe(3);
+        }
+
+        [Test]
+        public void Add_WhenInputIsSeparatedByAnewLine_ShouldReturnSum()
+        {
+            var result = Calculator.Add("1,2" + Environment.NewLine + "2");
+
+            result.ShouldBe(5);
+        }
+
+        [Test]
+        public void Add_WhenFirstLineContainsADelimiter_ShouldUseTheDelimiter()
+        {
+            var result = Calculator.Add("#|\n12|5\n1");
+
+            result.ShouldBe(18);
+        }
+    }
+}
---
+++ tdd.calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace tdd.calculator
+{
+    public static class Calculator
+    {
+        public static int Add(string input)
+        {
+            var value = 0;
+            if (input != null)
+            {
+                var firstline = 0;
+                var delimiters = ",";
+                if (input.StartsWith("#"))
+                {
+                    firstline = input.IndexOfAny(Environment.NewLine.ToCharArray());
+                    delimiters = input.Substring(1, firstline);
+                }
+
+                var numbers = input.Substring(firstline).Split((delimiters + Environment.NewLine).ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var number in numbers)
+                {
+                    var intVal = int.Parse(number);
+                    if (intVal < 0)
+                    {
+                        throw new Exception("Its negative!");
+                    }
+
+                    value += intVal;
+                }
+            }
+            return value;
+        }
+    }
+}
